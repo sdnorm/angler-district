26acdb1582cdef00069e0b57bdb9303e564ea101
sdnorm <spencerdnorman@gmail.com>
2016-12-21 14:02:03 -0600

    added header, side-nav, started footer . . .

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+  {name: "Lew's Speed Spool", description: "like new, buy this thing", price: 100.50},
+  {name: "Shimano Citica", description: "like new, buy this thing", price: 150.50},
+  {name: "Abu Garcia Revo", description: "like new, buy this thing", price: 2000.50},
+])
