589f6755e1547c72ab35cf099185b0b12f8b20a5
sdnorm <spencerdnorman@gmail.com>
2017-01-04 08:54:49 -0600

    made a slight change to push the new code

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_KEY'],
       :aws_secret_access_key  => ENV['S3_SECRET']
-      # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+      #:region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV['S3_BUCKET']
 end
