478a7f54e381351b0fe6e84daba0d463416e8052
sdnorm <spencerdnorman@gmail.com>
2017-02-16 20:46:36 -0600

    added omniauth route onto the default devise route

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,8 @@ Rails.application.routes.draw do
 
   resources :products
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
   get 'my-products', to: 'user_products#index', as: :all_user_products
   get 'edit-product/product/:id', to: 'products#edit', as: :edit_user_product
