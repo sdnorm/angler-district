d96a4cf6373e90772c88e5f586c6483cfca82aec
sdnorm <spencerdnorman@gmail.com>
2017-03-11 21:42:50 -0600

    attempted setting up a sign in as a dropdown and fixed the flash messages showing up for sign in and sign up stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,22 @@ class ApplicationController < ActionController::Base
     "cart#{current_user.id}"
   end
 
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def resource_class
+    User
+  end
+
+  helper_method :resource, :resource_name, :devise_mapping, :resource_class
+
 end
