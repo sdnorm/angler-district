97d1c564bfd41050ecadfcab7ec6531509eaa2e3
sdnorm <spencerdnorman@gmail.com>
2017-03-12 01:12:10 -0600

    added a custom file upload ui

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -26,3 +26,61 @@ $(window).load(function() {
     });
   });
 });
+
+$(function() {
+  var countFiles = 1,
+    $body = $('body'),
+    typeFileArea = ['txt', 'doc', 'docx', 'ods'],
+    coutnTypeFiles = typeFileArea.length;
+
+  //create new element
+  $body.on('click', '.files-wr label', function() {
+    var wrapFiles = $('.files-wr'),
+      newFileInput;
+
+    countFiles = wrapFiles.data('count-files') + 1;
+    wrapFiles.data('count-files', countFiles);
+
+    newFileInput = '<div class="one-file"><label for="file-' + countFiles + '">Attach file</label>' +
+      '<input type="file" name="file-' + countFiles + '" id="file-' + countFiles + '"><div class="file-item hide-btn">' +
+      '<span class="file-name"></span><span class="btn btn-del-file">x</span></div></div>';
+    wrapFiles.prepend(newFileInput);
+  });
+
+  //show text file and check type file
+  $body.on('change', 'input[type="file"]', function() {
+    var $this = $(this),
+      valText = $this.val(),
+      fileName = valText.split(/(\\|\/)/g).pop(),
+      fileItem = $this.siblings('.file-item'),
+      beginSlice = fileName.lastIndexOf('.') + 1,
+      typeFile = fileName.slice(beginSlice);
+
+    fileItem.find('.file-name').text(fileName);
+    if (valText != '') {
+      fileItem.removeClass('hide-btn');
+
+      for (var i = 0; i < coutnTypeFiles; i++) {
+
+        if (typeFile == typeFileArea[i]) {
+          $this.parent().addClass('has-mach');
+        }
+      }
+    } else {
+      fileItem.addClass('hide-btn');
+    }
+
+    if (!$this.parent().hasClass('has-mach')) {
+      $this.parent().addClass('error');
+    }
+  });
+
+  //remove file
+  $body.on('click', '.btn-del-file', function() {
+    var elem = $(this).closest('.one-file');
+    elem.fadeOut(400);
+    setTimeout(function() {
+      elem.remove();
+    }, 400);
+  });
+});
