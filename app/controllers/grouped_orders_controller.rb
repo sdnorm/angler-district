cf79c7380f61f3e39abb68f4e53c35eee74d4be2
sdnorm <spencerdnorman@gmail.com>
2017-03-21 14:25:22 -0500

    updated the sidebar maybe

--- app/controllers/grouped_orders_controller.rb
+++ app/controllers/grouped_orders_controller.rb
@@ -12,7 +12,7 @@ class GroupedOrdersController < ApplicationController
   def new
     @grouped_order = GroupedOrder.new
     @cart_ids = $redis.smembers current_user_cart
-    @products = Product.where(id: [@cart_ids])
+    @products = Product.where(slug: [@cart_ids])
     @total = @products.sum {|price| price.price}
   end
 
