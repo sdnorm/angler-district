1e229cdab901006d584a3762a7a9dbe4121cb86f
Spencer Norman <spencerdnorman@gmail.com>
2017-10-02 20:58:08 -0500

    updated a migration for order products and made a initializer for aws keys env vars

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY_ENV'],
-      :aws_secret_access_key  => ENV['S3_SECRET_ENV']
+      :aws_access_key_id      => ENV['S3_KEY_INT'],#ENV['S3_KEY_ENV'],
+      :aws_secret_access_key  => ENV['S3_SECRET_INT']#ENV['S3_SECRET_ENV']
       #:region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV['S3_BUCKET_ENV']
--- db/migrate/20170228231605_drop_order_products.rb
+++ db/migrate/20170228231605_drop_order_products.rb
@@ -1,5 +1,5 @@
 class DropOrderProducts < ActiveRecord::Migration[5.0]
   def change
-    drop_table :order_products
+    # drop_table :order_products
   end
 end
