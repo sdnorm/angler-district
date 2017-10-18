119f0e8300b590fda698b42c967f02816538d5a4
Spencer Norman <spencerdnorman@gmail.com>
2017-10-17 22:33:35 -0500

    made the carrierwave initializer envs match uncommented out seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,69 +1,69 @@
-# Brand.create([
-#   {name: "13 Fishing"},
-#   {name: "Abu Garcia"},
-#   {name: "Ardent"},
-#   {name: "Berkley"},
-#   {name: "Castaway"},
-#   {name: "Daiwa"},
-#   {name: "Denali Rods"},
-#   {name: "Dobyns Rods"},
-#   {name: "Duckett Fishing"},
-#   {name: "Falcon"},
-#   {name: "Fenwick"},
-#   {name: "G. Loomis"},
-#   {name: "Halo Fishing"},
-#   {name: "Jackall"},
-#   {name: "Lew's"},
-#   {name: "Megabass"},
-#   {name: "Okuma"},
-#   {name: "Quantum"},
-#   {name: "St. Croix"},
-#   {name: "Other"}
-# ])
-#
-# Brand.create([
-#   {name: "Offshore Angler"},
-#   {name: "Abu Garcia"},
-#   {name: "Penn"},
-#   {name: "Accurate"},
-#   {name: "Fin-Nor"},
-#   {name: "Daiwa"},
-#   {name: "Lew's"},
-#   {name: "Okuma"},
-#   {name: "Quantum"},
-#   {name: "Other"}
-# ])
+Brand.create([
+  {name: "13 Fishing"},
+  {name: "Abu Garcia"},
+  {name: "Ardent"},
+  {name: "Berkley"},
+  {name: "Castaway"},
+  {name: "Daiwa"},
+  {name: "Denali Rods"},
+  {name: "Dobyns Rods"},
+  {name: "Duckett Fishing"},
+  {name: "Falcon"},
+  {name: "Fenwick"},
+  {name: "G. Loomis"},
+  {name: "Halo Fishing"},
+  {name: "Jackall"},
+  {name: "Lew's"},
+  {name: "Megabass"},
+  {name: "Okuma"},
+  {name: "Quantum"},
+  {name: "St. Croix"},
+  {name: "Other"}
+])
 
-# Category.create([
-#   {name: "Reels"},
-#   {name: "Rods"},
-#   {name: "Lures"},
-#   {name: "General"},
-#   {name: "Apparel"},
-#   {name: "Boating"},
-#   {name: "Electronics"},
-#   {name: "Freshwater"},
-#   {name: "Saltwater"},
-# ])
-#
-# User.create([
-#   {email: "bob@example.com", password: "123456789"},
-#   {email: "larry@example.com", password: "123456789"},
-#   {email: "sam@example.com", password: "123456789"},
-#   {email: "joe@example.com", password: "123456789"},
-#   {email: "john@example.com", password: "123456789"}
-# ])
+Brand.create([
+  {name: "Offshore Angler"},
+  {name: "Abu Garcia"},
+  {name: "Penn"},
+  {name: "Accurate"},
+  {name: "Fin-Nor"},
+  {name: "Daiwa"},
+  {name: "Lew's"},
+  {name: "Okuma"},
+  {name: "Quantum"},
+  {name: "Other"}
+])
+
+Category.create([
+  {name: "Reels"},
+  {name: "Rods"},
+  {name: "Lures"},
+  {name: "General"},
+  {name: "Apparel"},
+  {name: "Boating"},
+  {name: "Electronics"},
+  {name: "Freshwater"},
+  {name: "Saltwater"},
+])
+
+User.create([
+  {email: "bob@example.com", password: "123456789"},
+  {email: "larry@example.com", password: "123456789"},
+  {email: "sam@example.com", password: "123456789"},
+  {email: "joe@example.com", password: "123456789"},
+  {email: "john@example.com", password: "123456789"}
+])
 
 file  = File.open(File.join(Rails.root,'app/assets/images/rs (4).jpeg'))
 #
 Product.create([
-  # {
-  #   name: "Lew's Speed Spool",
-  #   description: "Bacon ipsum dolor amet ribeye biltong pork chuck. Shankle meatloaf porchetta bacon short ribs swine kielbasa, corned beef turkey. Leberkas tri-tip landjaeger pork. Landjaeger turkey cupim sirloin short loin rump, chuck tongue.",
-  #   price: 100.50,
-  #   user_id: 5,
-  #   display_image: file
-  # },
+  {
+    name: "Lew's Speed Spool",
+    description: "Bacon ipsum dolor amet ribeye biltong pork chuck. Shankle meatloaf porchetta bacon short ribs swine kielbasa, corned beef turkey. Leberkas tri-tip landjaeger pork. Landjaeger turkey cupim sirloin short loin rump, chuck tongue.",
+    price: 100.50,
+    user_id: 5,
+    display_image: file
+  },
   {
     name: "Shimano Citica",
     description: "Bacon ipsum dolor amet ribeye biltong pork chuck. Shankle meatloaf porchetta bacon short ribs swine kielbasa, corned beef turkey. Leberkas tri-tip landjaeger pork. Landjaeger turkey cupim sirloin short loin rump, chuck tongue.",
