af0a43456d3dc411c0712acb5948f66d302b9866
Spencer Norman <spencerdnorman@gmail.com>
2017-10-25 21:21:11 -0500

    took out some code that wasnt needed and made the hamburger menu bigger

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
-// //= require masonry.pkgd
 //= require jquery
 //= require jquery_ujs
 //= require foundation
@@ -6,27 +5,3 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
-
-$(function($){
-  $.fn.masonry = function(params){
-    var defaults = {
-      columns: 3
-    };
-
-    var options = $.extend(defaults, params),
-        container = this,
-        items = container.find('.item'),
-        colCount = 0,
-        columns = $(Array(options.columns + 1).join('<div></div>')).addClass('masonryColumn').appendTo(container);
-    for(var c = 0; c < items.length; c++){
-      items.eq(c).appendTo(columns.eq(colCount));
-      colCount = (colCount + 1 > (options.columns - 1)) ? 0 : colCount + 1;
-    }
-  }
-}(jQuery));
-
-$(function(){
-  $('.wrapper').masonry({
-    columns: 4
-  });
-});
