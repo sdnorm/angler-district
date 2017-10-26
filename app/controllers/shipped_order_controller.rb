a9c9ade4ef26252b86f3fbba84bde56a3ae7ed46
Spencer Norman <spencerdnorman@gmail.com>
2017-10-26 13:46:49 -0500

    fixing up some order pages

--- app/controllers/shipped_order_controller.rb
+++ app/controllers/shipped_order_controller.rb
@@ -1,7 +1,13 @@
 class ShippedOrderController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
+    @orders = Order.shipped(current_user)
   end
 
   def show
+
   end
+
 end
--- app/controllers/to_ship_order_controller.rb
+++ app/controllers/to_ship_order_controller.rb
@@ -1,7 +1,13 @@
 class ToShipOrderController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
+    @orders = Order.to_ship(current_user)
   end
 
   def show
+    
   end
+
 end
