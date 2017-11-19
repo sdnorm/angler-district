b94a2bd855b0ea6d9d22c475781bfda2f8d221e1
Spencer Norman <spencerdnorman@gmail.com>
2017-11-19 13:14:37 -0600

    trying to get this js shiz figured out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//=require side_menu
 //= require_tree .
 
 // $(function(){ $(document).foundation(); });
-
 $(document).foundation();
-
-
-function openMobileSignedNav() {
-  document.getElementById("signedMobileNav").style.width = "300px";
-}
-
-function closeMobileSignedNav() {
-  document.getElementById("signedMobileNav").style.width = "0";
-}
