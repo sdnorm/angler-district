34f695147900144198f8e1b6ab869f73406ae635
Spencer Norman <spencerdnorman@gmail.com>
2017-11-19 14:46:09 -0600

    trying to clean up the side nav stuff

--- app/assets/javascripts/side_menu.js
+++ app/assets/javascripts/side_menu.js
@@ -1,31 +1,15 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("side-nav").style.width = "300px";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("side-nav").style.width = "0";
 }
 
 function openSignedNav() {
-  document.getElementById("signedSideNav").style.width = "300px";
+  document.getElementById("signed-side-nav").style.width = "300px";
 }
 
 function closeSignedNav() {
-  document.getElementById("signedSideNav").style.width = "0";
-}
-
-function openMobileNav() {
-  document.getElementById("mobileNav").style.width = "300px";
-}
-
-function closeMobileNav() {
-  document.getElementById("mobileNav").style.width = "0";
-}
-
-function openMobileSignedNav() {
-  document.getElementById("signedMobileNav").style.width = "300px";
-}
-
-function closeMobileSignedNav() {
-  document.getElementById("signedMobileNav").style.width = "0";
+  document.getElementById("signed-side-nav").style.width = "0";
 }
